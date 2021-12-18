10d970de0ea0d95ca3d37894e2b06deda4f29374
akilany <ammyass2@gmail.com>
2021-12-18 12:57:42 +0200

    fix: Missing the type information for parameter and function return value.

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,9 +1,10 @@
 import express from 'express'
+import { Request, Response } from 'express'
 import imageRouter from './api/imageRouter'
 
 const router = express.Router()
 
-router.route('/').get((req, res) => {
+router.route('/').get((req: Request, res: Response): void => {
   res
     .status(200)
     .send('<h1>Main API Route</h1><a href="/api/image">Visit Image API</a>')
--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,6 @@
 import path from 'path'
 import express from 'express'
+import { Request, Response } from 'express'
 import morgan from 'morgan'
 import router from './routes/index'
 
@@ -7,12 +8,11 @@ const app = express()
 const PORT = 3333
 
 app.use(express.static(path.join(__dirname, '../public')))
-
 app.use(morgan('dev'))
 
+// Router
 app.use('/api', router)
-
-app.get('/', (req, res) => {
+app.get('/', (req: Request, res: Response): void => {
   res
     .status(200)
     .send(
@@ -20,6 +20,7 @@ app.get('/', (req, res) => {
     )
 })
 
+// Server
 app.listen(PORT, () => {
   console.log(`Server is listning on port: http://localhost:${PORT}`)
 })
